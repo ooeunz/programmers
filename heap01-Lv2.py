dc8e7a81df1298ab8b26d48257e6c77b92f4ee3a
ooeunz <yuns994@gmail.com>
2019-09-10 00:31:51 +0900

    heap01 sucess

--- heap01-Lv2.py
+++ heap01-Lv2.py
@@ -4,15 +4,20 @@ K = 7
 def solution(scoville, K):
     heapq.heapify(scoville)
     ans = 0
-    while scoville[0] <K:
-        # make
-        first = heapq.heappop(scoville)
-        second = heapq.heappop(scoville)
-        mix = first + second*2
+    if len(scoville) <= 1:
+        return -1
+    else:
+        while scoville[0] <K:
+            if len(scoville) <= 1:
+                return -1
+            # make
+            first = heapq.heappop(scoville)
+            second = heapq.heappop(scoville)
+            mix = first + second*2
 
-        # push
-        heapq.heappush(scoville, mix)
-        ans += 1
+            # push
+            heapq.heappush(scoville, mix)
+            ans += 1
     return ans
 
 if __name__ == "__main__":
