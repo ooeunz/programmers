df465f567149e8f92feb99df4fbbb21b6ea915f0
ooeunz <yuns994@gmail.com>
2019-09-10 12:21:05 +0900

    heap02 sucess

---
+++ heap02-Lv2.py
@@ -0,0 +1,19 @@
+import heapq
+
+no = 4
+works = [4, 3, 3]
+def solution(no, works):
+    last = len(works)-1
+
+    while no:
+        works.sort()
+        if works[last]:
+            works[last] -= 1
+        no -= 1
+    result  = 0
+    for i in works:
+        result += i ** 2
+
+    return result
+
+print(solution(no, works))
