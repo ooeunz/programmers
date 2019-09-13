d3a247f90edc2de54a40720b95520b95cd11ce05
ooeunz <yuns994@gmail.com>
2019-09-13 16:48:57 +0900

    hash04-Lv3 sucess spending time : 8

--- hash04-Lv3.py
+++ hash04-Lv3.py
@@ -1,50 +1,58 @@
 # 방문 길이
-def can(x, y):
-    if x < 0 or x >10:
+def can(xy_next, move_x, move_y):
+    x = xy_next[0] + move_x
+    y = xy_next[1] + move_y
+    if x <= 0 or x > 11:   
         return True
-    elif y < 0 or y> 10:
+    if y <= 0 or y > 11:   
         return True
 
-def move(x, y, room):
-    if room[y][x] == True:     # 갔던 길이라면
+def move(xy, xy_next, move_x, move_y, course):
+    
+    # print("move   : {}".format(xy_next))    #debug
+    if can(xy_next, move_x, move_y):    # out of range check
+        return False
+    xy_next[0] += move_x
+    xy_next[1] += move_y
+    convert = list(map(str, xy))
+    convert_next = list(map(str, xy_next))
+    position = "({},{})-({},{})".format(convert[0], convert[1], convert_next[0], convert_next[1])
+    position_reverse = "({},{})-({},{})".format(convert_next[0], convert_next[1], convert[0], convert[1])
+    xy[0], xy[1] = xy_next
+
+    # print(position) # debug
+    if position in course or position_reverse in course:
+        return False
+    else:
+        course[position] = True
+        return True
+
+def action(xy, xy_next, move_x, move_y, course):
+    # print("action : {}".format(xy_next))   # debug
+    if move(xy, xy_next, move_x, move_y, course) == False:
         return 0
-    elif room[y][x] == False:    # 처음가는 길이라면
-        room[y][x] = True
+    else:
         return 1
 
 def solution(dirs):
     # initialization
-    room = [[False for w in range(11)]for h in range(11)]
-    person = {"x" : 5, "y" : 5}
-    room[5][5] = True
-    distance = 0
+    xy = [6, 6]
+    xy_next = [6, 6]
+    course = {}
+    count = 0
 
     for dir in dirs:
         if dir == "U":
-            person["y"] -= 1
-            if can(person["x"], person["y"]):
-                continue
-            else:
-                distance += move(person["x"], person["y"], room)
+            count += action(xy, xy_next, 0, -1, course)
         elif dir == "D":
-            person["y"] += 1
-            if can(person["x"], person["y"]):
-                continue
-            else:
-                distance += move(person["x"], person["y"], room)
-        elif dir == "L":
-            person["x"] -= 1
-            if can(person["x"], person["y"]):
-                continue
-            else:
-                distance += move(person["x"], person["y"], room)
+            count += action(xy, xy_next, 0, 1, course)
         elif dir == "R":
-            person["x"] += 1
-            if can(person["x"], person["y"]):
-                continue
-            else:
-                distance += move(person["x"], person["y"], room)
-    return distance
+            count += action(xy, xy_next, 1, 0, course)
+        elif dir == "L":
+            count += action(xy, xy_next, -1, 0, course)
+    return count
+
+            
 
 if __name__ == "__main__":
     print(solution("ULURRDLLU"))
--- test.py
+++ test.py
@@ -1,14 +1,57 @@
-# 값 변경 안됨
-num = [1, 2, 3]
-for i in num:
-    i += 1
-print(num)
-# print => [1, 2, 3]
+# 방문 길이
+def can(x, y):
+    if x < 0 or x >10:
+        return True
+    elif y < 0 or y> 10:
+        return True
 
-# 값 변경됨
-nums = [[1, 2, 3], [4, 5, 6]]
-for i in nums:
-    i[0] += 1
-print(nums)
+def move(x, y, room):
+    if room[y][x] == True:     # 갔던 길이라면
+        return 0
+    elif room[y][x] == False:    # 처음가는 길이라면
+        room[y][x] = True
+        return 1
 
-# print => [[2, 2, 3], [5, 5, 6]]
+def solution(dirs):
+    # initialization
+    room = [[False for w in range(11)]for h in range(11)]
+    person = {"x" : 5, "y" : 5}
+    room[5][5] = True
+    distance = 0
+
+    for dir in dirs:
+        if dir == "U":
+            person["y"] -= 1
+            if can(person["x"], person["y"]):
+                continue
+            else:
+                distance += move(person["x"], person["y"], room)
+        elif dir == "D":
+            person["y"] += 1
+            if can(person["x"], person["y"]):
+                continue
+            else:
+                distance += move(person["x"], person["y"], room)
+        elif dir == "L":
+            person["x"] -= 1
+            if can(person["x"], person["y"]):
+                continue
+            else:
+                distance += move(person["x"], person["y"], room)
+        elif dir == "R":
+            person["x"] += 1
+            if can(person["x"], person["y"]):
+                continue
+            else:
+                distance += move(person["x"], person["y"], room)
+    return distance
+
+if __name__ == "__main__":
+    print(solution("ULURRDLLU"))
+    print(solution("LULLLLLLU"))
+        
+
+
+
+
+
