3f4ba6b43c8ade2f68cda42a89d5ccc977916e37
ooeunz <yuns994@gmail.com>
2019-09-22 17:52:59 +0900

    search03-Lv2

--- search03-Lv3.py
+++ search03-Lv3.py
@@ -1,30 +1,25 @@
 from itertools import product
 # 주사위 게임
-def solution(monster, S1, S2, S3):
+def solution(monsters, S1, S2, S3):
     # initialization
     max_dice = S1 + S2 + S3 + 1
-    field = [True] * (max_dice + 2)
+    field = [True] * (max_dice + 99)
     count = 0
-    for i in monster:
-        field[i] = False
+    for monster in monsters:
+        field[monster] = False
 
     S1 = [i for i in range(1, S1+1)]
     S2 = [i for i in range(1, S2+1)]
     S3 = [i for i in range(1, S3+1)]
 
     dice_case = list(map(sum, list(product(S1, S2, S3))))
-    
+
     for dice in dice_case:
-        if field[dice] == True:
+        if field[dice+1] == True:
             count += 1
     
-    return count / len(dice_case)
-
-
-
-
-
+    return int(count / len(dice_case) * 1000)
 
 if __name__ == "__main__":
-    # print(solution([4, 9, 5, 8], 2, 3, 4))
+    print(solution([4, 9, 5, 8], 2, 3, 4))
     print(solution([4, 9, 5, 8], 2, 3, 3))
