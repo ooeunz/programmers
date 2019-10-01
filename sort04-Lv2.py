83bd6c70197edb0a6ef51bb47bfa6ba7d566d226
ooeunz <yuns994@gmail.com>
2019-10-01 23:03:56 +0900

    dp01

--- sort04-Lv2.py
+++ sort04-Lv2.py
@@ -1,12 +1,11 @@
 # 가장 큰 수
 from itertools import permutations
 def solution(numbers):
-    numbers = [str(number) for number in numbers]
-    ans = []
-    for number in permutations(numbers):
-        ans.append(''.join(number))
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*4, reverse=True)
+    ans = ''.join(number for number in numbers)
     
-    return max(ans)
+    return str(int(ans))
 if __name__ == "__main__":
     print(solution([6, 10, 2]))
     print(solution([3, 30, 34, 5, 9]))
