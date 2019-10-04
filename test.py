d8b3190e30e4e650c8c7d4a499e403aefec263e6
ooeunz <yuns994@gmail.com>
2019-10-04 23:08:40 +0900

    test

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-import pygame
-import sys
-
-screen = pygame.display.set_mode((500, 500))
-
-while 1:
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            sys.quit()
-
-    screen.fill((255, 255, 0))
-
-    pygame.display.update()
+# test file
