20919a0ff2657699e119f82f0f2ab03a22da7d70
ooeunz <yuns994@gmail.com>
2020-02-13 11:46:04 +0900

    코드 수정

--- hash/spy.py
+++ hash/spy.py
@@ -3,8 +3,7 @@ from functools import reduce
 
 def solution(clothes):
     cnt = Counter([category for name, category in clothes])
-    ans = reduce(lambda x, y: x*(y+1), cnt.values(), 1) - 1
-    return ans
+    return reduce(lambda x, y: x*(y+1), cnt.values(), 1) - 1
 
 if __name__ == "__main__":
     print(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]))
