f50c4fa2d826b002ffc51ce4d2f35e95281210e3
ooeunz <yuns994@gmail.com>
2020-04-14 09:31:27 +0900

    compress_str

--- 2020_official/compress_str.py
+++ 2020_official/compress_str.py
@@ -1,28 +1,28 @@
 def compress(ss: str, num: int):
     ans = ''
+    cnt = 1
 
     for i in range(num, len(ss), num):
         std = ss[i-num:i]
-        cnt = 1
 
         if std == ss[i:i+num]:
             cnt += 1
         else:
-            if cnt > 1:
+            if cnt == 1:
+                ans += std
+            else:
                 ans += str(cnt) + std
                 cnt = 1
-            else:
-                ans += std
 
     if len(ss[i:num+i]) == num:
-        if cnt > 1:
-            ans += str(cnt) + std
-        else:
+        if cnt == 1:
             ans += std
+        else:
+            ans += str(cnt) + std
     else:
         ans += ss[i:num+i]
 
-    print(ans)
+    # print(ans)
     return len(ans)
 
 
@@ -31,39 +31,15 @@ def solution(ss: str):
         return 1
 
     ans = float('inf')
-    for i in range(1, len(ss) // 2):
+    for i in range(1, len(ss)):
         ans = min(ans, compress(ss, i))
 
     return ans
 
-# def solution(s):
-#     answers = []
-#     if len(s) == 1:
-#         return 1
-#     for i in range(1, len(s)):
-#         answer = ''
-#         count = 1
-#         for j in range(i, len(s), i):
-#             if s[j-i:j] == s[j:j+i]:
-#                 count += 1
-#             else:
-#                 if count == 1:
-#                     answer += s[j-i:j]
-#                 else:
-#                     answer += str(count)+s[j-i:j]
-#                     count = 1
-#         if len(s[j:j+i]) == i:
-#             if count == 1:
-#                 answer += s[j-i:j]
-#             else:
-#                 answer += str(count)+s[j-i:j]
-#                 count = 1
-#         else:
-#             answer += s[j:j+i]
-#         answers.append(len(answer))
-#     return min(answers)
-
 
 if __name__ == "__main__":
-    print(solution("aabbaccc"))
-    # print(solution("ababcdcdababcdcd"))
+    print(solution("aabbaccc"))  # 7
+    print(solution("ababcdcdababcdcd"))  # 9
+    print(solution("abcabcdede"))   # 8
+    print(solution("abcabcabcabcdededededede"))  # 14
+    print(solution("xababcdcdababcdcd"))    # 17
