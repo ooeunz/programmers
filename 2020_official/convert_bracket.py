d2f385ed30c48208fed81e6cd163f551fc1a5f2b
ooeunz <yuns994@gmail.com>
2020-04-14 09:33:00 +0900

    convert_bracket

---
+++ 2020_official/convert_bracket.py
@@ -0,0 +1,9 @@
+def solution(p):
+    answer = ''
+    return answer
+
+
+if __name__ == "__main__":
+    print(solution("(()())()"))  # "(()())()"
+    print(solution(")("))   # "()"
+    print(solution("()))((()")  # "()(())()"
