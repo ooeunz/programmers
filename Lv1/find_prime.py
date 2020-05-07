8ac6e65e0995e6d604d12f23765b5944cefbf40f
ooeunz <yuns994@gmail.com>
2020-05-07 18:20:51 +0900

    풀지못함

--- Lv1/find_prime.py
+++ Lv1/find_prime.py
@@ -1,12 +1,11 @@
-def solution(n):
-
-    a = [False, False] + [True]*(n-1)
+def solution(n: int):
+    a = [False, False] + [True] * (n-1)
     primes = []
 
     for i in range(2, n+1):
         if a[i]:
             primes.append(i)
-            for j in range(2*i, n+1, i):
+            for j in range(i*2, n+1, i):
                 a[j] = False
     return len(primes)
 
---
+++ Lv3/line_way.py
@@ -0,0 +1,14 @@
+import math
+
+
+def solution(n: int, k: int):
+    people = [i for i in range(1, n+1)]
+
+
+# 풀이: http://blog.naver.com/PostView.nhn?blogId=jwyoon25&logNo=221347789536&categoryNo=0&parentCategoryNo=0&viewDate=&currentPage=1&postListTopCurrentPage=1&from=postView&userTopListOpen=true&userTopListCount=5&userTopListManageOpen=false&userTopListCurrentPage=1
+# 순열: https://shoark7.github.io/programming/algorithm/Permutations-and-Combinations
+# 팩토리얼: https://shoark7.github.io/programming/algorithm/several-ways-to-solve-factorial-in-python
+
+if __name__ == "__main__":
+    print(solution(3, 5))
+    print(solution(20, 20))
