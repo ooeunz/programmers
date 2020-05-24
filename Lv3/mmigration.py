f102e0f12075d55b14e4cd7318f37f5e954a7e10
ooeunz <yuns994@gmail.com>
2020-05-24 14:24:02 +0900

    immigration

---
+++ Lv3/mmigration.py
@@ -0,0 +1,18 @@
+def solution(n: int, times: list):
+    times.sort()
+
+    start, end = 1, max(times) * n
+    ans = end
+
+    while start <= end:
+        mid = (start + end) // 2
+        if n > sum(map(lambda x: mid//x, times)):
+            start = mid + 1
+        else:
+            ans = min(mid, ans)
+            end = mid - 1
+            
+    return ans
+
+if __name__ == "__main__":
+    print(solution(6, [7, 10]))
