08414c2bd626ee35e2409492d49236a8df61d9f6
ooeunz <yuns994@gmail.com>
2020-05-25 21:18:21 +0900

    ttangttameokgi

--- Lv2/ttangttameokgi.py
+++ Lv2/ttangttameokgi.py
@@ -1,10 +1,11 @@
 def solution(land: list):
-    ans = 0
-    memory = 4
-    for l in land:
-        memory, v = max([(i, v) for i, v in enumerate(l) if i != memory], key=lambda x: x[1])
-        ans += v
-    return ans
+    for i in range(1, len(land)):
+        land[i][0] += max(land[i-1][1], land[i-1][2], land[i-1][3])
+        land[i][1] += max(land[i-1][0], land[i-1][2], land[i-1][3])
+        land[i][2] += max(land[i-1][0], land[i-1][1], land[i-1][3])
+        land[i][3] += max(land[i-1][0], land[i-1][1], land[i-1][2])
+
+    return max(land[-1])
 
 if __name__ == "__main__":
     print(solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]]))
